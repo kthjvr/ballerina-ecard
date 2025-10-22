c7044ddddd35fb8f03639f4a4bad6f4b31083dda
Kathy <kathleen.jvrp@gmail.com>
2025-10-22 19:20:53 +0800

    storybook update

--- config/event-details.js
+++ config/event-details.js
@@ -19,7 +19,7 @@ export const EVENT_CONFIG = {
     venueAddress: "Bolbok, Lipa City, Batangas",
     venueAddressFull: "W5J2+7V3, Bolbok, Lipa City, Batangas, Philippines",
     mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3872.475297025878!2d121.15194350000002!3d13.9302817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd6cb421414f1d%3A0x46bf47272446e579!2sGreen%20Leaf!5e0!3m2!1sen!2sph!4v1761127397455!5m2!1sen!2sph",
-    nearbyLandmarks: "---",
+    nearbyLandmarks: "Capuchin Retreat Center",
 
     // Contact
     googleFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSfVJDQGwyQ2Ce1TIaJsLjp-neMOTRsKmQECSKIUS49a6FuaMA/viewform",
--- data/storybook.js
+++ data/storybook.js
@@ -7,32 +7,32 @@ export const storyBookData = [
     },
     {
         title: "My First Dance!",
-        image: "/assets/story/first.png",
+        image: "/assets/gallery/first.jpg",
         text: "One year old and already stealing the show! My first birthday was filled with tiny tutus, baby steps, and the beginning of something magical. The stage was set for a lifetime of beautiful moments!"
     },
     {
         title: "Two Twirls of Joy!",
-        image: "/assets/story/second.png",
+        image: "/assets/gallery/second.jpg",
         text: "At two, I'm learning to spin and leap! Two candles on my cake and twice the giggles. Every day is a new dance, and I'm discovering the rhythm of life with every little step!"
     },
     {
         title: "Three Graceful Years!",
-        image: "/assets/story/third.png",
+        image: "/assets/gallery/third.jpg",
         text: "Three years of pirouettes and plies! I can twirl on my tiptoes (well, almost!) and my tutus are getting fluffier. Watch me dance through this magical year of growing and glowing!"
     },
     {
         title: "Four Fabulous Leaps!",
-        image: "/assets/story/fourth.png",
+        image: "/assets/gallery/fourth.jpg",
         text: "Four candles dancing like spotlights on my stage! I'm practicing my grand jetés and dreaming bigger dreams. Every birthday is a new performance, and this one is my best encore yet!"
     },
     {
         title: "Fifth Position at Five!",
-        image: "/assets/story/fifth.png",
+        image: "/assets/gallery/fifth.jpg",
         text: "Five years of twirling through life! I've mastered my ballet positions, and my arabesques are getting higher. With ribbon in my hair and magic in my feet, I'm ready to dance into new adventures!"
     },
     {
         title: "Six and En Pointe!",
-        image: "/assets/story/sixth.png",
+        image: "/assets/gallery/sixth.jpg",
         text: "Six wonderful years of birthdays and ballet! I'm a little ballerina with big dreams, spinning through life with grace and sparkles. Each year is another beautiful chapter in my dance story!"
     },
     {
